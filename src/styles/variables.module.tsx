bdc43483261203d393e58dff86ba3e9a4a79562a
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2023-07-11 10:45:35 -0300

    Create variables.module.tsx

---
+++ src/styles/variables.module.tsx
@@ -0,0 +1,13 @@
+export const themes = {
+    light: {
+      backgroundOne: '#d5d4d4',
+      backgroundTwo: '#cecdcd',
+      fontColor: 'black',
+    },
+    dark: {
+      backgroundOne: '#282828',
+      backgroundTwo: '#0f0f0f',
+      fontColor: '#d7d6d6',
+    }
+  };
+  
