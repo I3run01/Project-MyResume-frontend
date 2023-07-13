38f96fea89ee254e6d1b2380ef379cfc9ae026dd
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2023-07-13 17:20:59 -0300

    Update leftMenu.tsx

--- src/components/leftMenu/leftMenu.tsx
+++ src/components/leftMenu/leftMenu.tsx
@@ -3,24 +3,20 @@ import { LeftMenuDiv } from './leftMenu.module'
 import { RootState } from '@/redux/store'
 import { changeTheme } from '@/redux/slice/themeSlice'
 import { useDispatch } from 'react-redux'
+import { useState } from 'react'
 
 export const LeftMenu = () => {
     const isDark = useSelector((state: RootState) => state.theme.isDark)
     const dispatch = useDispatch()
+    const [menuIsOpened, setMenuIsOpened] = useState<boolean>(true)
 
     return (
         <LeftMenuDiv
         isDark = {isDark}
         >
-            <div className='options'>
-                <div className='openAndCloseMenuButton'>
-
-                </div>
-                
-                <div>Resume</div>
-                <div>Skills</div>
-                <div>Experience</div>
-                <div>Projects</div>
+            <div className='options'>            
+                <div>Curriculum</div>
+                <div>Project</div>
             </div>
             <div className='settings' onClick={() => dispatch(changeTheme())}>
                 <p>change theme</p>
