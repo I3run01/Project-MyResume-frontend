6155334329e37e8d24df360a809624cd89356b49
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2023-07-19 18:51:39 -0300

    project router created

--- src/components/leftMenu/leftMenu.tsx
+++ src/components/leftMenu/leftMenu.tsx
@@ -4,19 +4,20 @@ import { RootState } from '@/redux/store'
 import { changeTheme } from '@/redux/slice/themeSlice'
 import { useDispatch } from 'react-redux'
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 export const LeftMenu = () => {
     const isDark = useSelector((state: RootState) => state.theme.isDark)
+    const router = useRouter()
     const dispatch = useDispatch()
-    const [menuIsOpened, setMenuIsOpened] = useState<boolean>(true)
 
     return (
         <LeftMenuDiv
         isDark = {isDark}
         >
             <div className='options'>         
-                <div>Curriculum</div>
-                <div>Project</div>
+                <div onClick={() => router.push('./curriculum')}>Curriculum</div>
+                <div onClick={() => router.push('./project')}>Project</div>
             </div>
             <div className='settings' onClick={() => dispatch(changeTheme())}>
                 <p>change theme</p>
---
+++ src/pages/dashboard/project.tsx
@@ -0,0 +1,17 @@
+import { DashboardLayout } from '@/layouts/dashboardLayout/dashboardLayout'
+
+const Project = () => {
+
+    return (
+        <DashboardLayout
+            main={
+                <>
+                    <h1>COMMING SOON</h1>
+                </>
+            }
+        />
+    )
+
+}
+
+export default Project
